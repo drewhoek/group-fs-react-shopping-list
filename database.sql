69a9e806015743e73a4d4606510a2e35e7c37297
Brock Benson <brockjbenson@outlook.com>
2023-01-26 11:28:21 -0600

    db created along with all of our db query syntax

--- database.sql
+++ database.sql
@@ -1,2 +1,24 @@
 -- Don't forget to add your create table SQL 
+
+CREATE TABLE shopping_list (
+	id SERIAL PRIMARY KEY,
+	item varchar(25),
+	quantity integer,
+	unit varchar(25),
+	is_purchased boolean default false
+);
+
+INSERT INTO shopping_list (item, quantity, unit) 
+VALUES ('Apples', 5, 'lbs'),
+		('Bread', 1, 'Loaf'),
+		('Milk', 1, 'Gallon');
+
+UPDATE shopping_list SET is_purchased = true WHERE id = id;
+
+DELETE FROM shopping_list WHERE id = id;
+
+DELETE FROM shopping_list;
+
+UPDATE shopping_list SET is_purchased = false;
+
 -- It is also helpful to include some test data
