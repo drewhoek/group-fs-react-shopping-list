6e38c093e0f6e5d321d5d58830a723a9c277a355
Brock Benson <brockjbenson@outlook.com>
2023-01-26 16:30:50 -0600

    style

--- src/components/ShoppingList/ShoppingList.jsx
+++ src/components/ShoppingList/ShoppingList.jsx
@@ -1,27 +1,25 @@
-
-
-export function ShoppingList ({shoppingList, handleRemove, removeAll}) {
-
-    return (
-        <>
-        <div className="header2">
+export function ShoppingList({ shoppingList, handleRemove, removeAll }) {
+  return (
+    <>
+      <div className="header2">
         <h2>Shopping List</h2>
         <button>Reset</button>
         <button onClick={() => removeAll()}>Clear</button>
-        </div>
-        <div className="list">
-            {shoppingList.map((val) => (
-                <div key={val.id} className="list-items">
-                    <p className="item-name">{val.item}</p>
-                    <p>Item Quantity: {val.quantity} {val.unit}</p>
-                    <div className="list-btns">
-                    <button>Buy</button>
-                    <button onClick={() => handleRemove(val.id)}>Remove</button>
-                    </div>
-                </div>
-            )
-            )}
-        </div>
-        </>
-    )
+      </div>
+      <div className="list">
+        {shoppingList.map((val) => (
+          <div key={val.id} className="list-items">
+            <p className="item-name">{val.item}</p>
+            <p className="item-qaun">
+              Item Quantity: {val.quantity} {val.unit}
+            </p>
+            <div className="list-btns">
+              <button>Buy</button>
+              <button onClick={() => handleRemove(val.id)}>Remove</button>
+            </div>
+          </div>
+        ))}
+      </div>
+    </>
+  );
 }
