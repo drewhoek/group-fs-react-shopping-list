94818b1052b7f800bd5e7e84e68139e0ff0afc33
Brock Benson <brockjbenson@outlook.com>
2023-01-26 15:48:55 -0600

    post and get good to go

--- src/components/ShoppingListForm/ShoppingListForm.jsx
+++ src/components/ShoppingListForm/ShoppingListForm.jsx
@@ -1,12 +1,28 @@
-export function ShoppingListForm () {
+export function ShoppingListForm ({ addItem, setNewItem, newItem, newQuan, setNewQuan, newUnit, setNewUnit }) {
+
+    function handleSave (e) {
+
+        e.preventDefault();
+        addItem();
+    }
 
     return (
         <>
         <h2>Add Item</h2>
-        <input type="text" placeholder="Item" />
-        <input type="number" placeholder="Quantity" />
-        <input type="text" placeholder="Unit" />
-        <button>Save</button>
+        <form onSubmit={handleSave}>
+        <input type="text"
+          placeholder="Item"
+          value={newItem}
+          onChange={(evt) => setNewItem(evt.target.value)}/>
+        <input type="number"
+         placeholder="Quantity"
+         value={newQuan}
+         onChange={(evt) => setNewQuan(evt.target.value)}/>
+        <input type="text" placeholder="Unit"
+           onChange={(evt) => setNewUnit(evt.target.value)}
+           value={newUnit} />
+        <button type="submit">Save</button>
+        </form>
         </>
     )
 }
