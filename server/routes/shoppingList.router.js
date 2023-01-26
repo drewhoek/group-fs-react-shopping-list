edc33a23522a57a992fae62739d90f756d8739e4
Drew Hoek <drewh815@gmail.com>
2023-01-26 13:53:09 -0600

    Delete route working

--- server/routes/shoppingList.router.js
+++ server/routes/shoppingList.router.js
@@ -3,45 +3,52 @@ const router = express.Router();
 const pool = require("../modules/pool");
 
 router.get("/", (req, res) => {
-  console.log("in GET shopping list");
-  const queryText = `SELECT * FROM "shopping_list";`;
+    console.log("in GET shopping list");
+    const queryText = `SELECT * FROM "shopping_list";`;
 
-  pool
-    .query(queryText)
-    .then((result) => {
-      console.log("successful select from database");
-      res.send(result.rows);
-    })
-    .catch((error) => {
-      console.log(`error ${queryText}`, error);
-      res.sendStatus(505);
-    });
+    pool
+        .query(queryText)
+        .then((result) => {
+            console.log("successful select from database");
+            res.send(result.rows);
+        })
+        .catch((error) => {
+            console.log(`error ${queryText}`, error);
+            res.sendStatus(505);
+        });
 });
 
 router.post("/", (req, res) => {
-  console.log("in post", req.body);
-  const item = req.body.item;
-  const quantity = req.body.quantity;
-  const unit = req.body.unit;
+    console.log("in post", req.body);
+    const item = req.body.item;
+    const quantity = req.body.quantity;
+    const unit = req.body.unit;
 
-  if (!item || !quantity || !unit) {
-    const errorMessage = "error message";
-    console.log(errorMessage);
-    res.status(400).send(errorMessage);
-  }
+    if (!item || !quantity || !unit) {
+        const errorMessage = "error message";
+        console.log(errorMessage);
+        res.status(400).send(errorMessage);
+    }
 
-  const queryText = `INSERT INTO shopping_list (item, quantity, unit)
+    const queryText = `INSERT INTO shopping_list (item, quantity, unit)
                         VALUES ($1, $2, $3);`;
 
-  pool
-    .query(queryText, [item, quantity, unit])
-    .then((response) => {
-      res.sendStatus(201);
-    })
-    .catch((error) => {
-      console.log(`error ${queryText}`, error);
-      res.sendStatus(500);
-    });
+    pool
+        .query(queryText, [item, quantity, unit])
+        .then((response) => {
+            res.sendStatus(201);
+        })
+        .catch((error) => {
+            console.log(`error ${queryText}`, error);
+            res.sendStatus(500);
+        });
 });
+
+router.delete("/:id", (req, res) => {
+    pool
+        .query(`DELETE FROM "shopping_list" WHERE "id"=$1;`, [req.params.id])
+        .then(() => res.sendStatus(204))
+        .catch((err) => res.sendStatus(500));
+})
 
 module.exports = router;
