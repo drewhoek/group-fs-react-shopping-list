c301a4f50307a6dd7c9d80160c25bfd2e9d2dc80
Drew Hoek <drewh815@gmail.com>
2023-01-27 10:24:56 -0600

    Mark all as not purchased working as intended

--- src/components/ShoppingList/ShoppingList.jsx
+++ src/components/ShoppingList/ShoppingList.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 
-export function ShoppingList({ shoppingList, handleRemove, removeAll }) {
+export function ShoppingList({ shoppingList, handleRemove, removeAll, markPurchased, markAllNotPurchased }) {
   const [buttonClass, setButtonClass] = useState("list-btns");
   const [purchasedText, setPurchasedText] = useState("");
 
@@ -8,7 +8,7 @@ export function ShoppingList({ shoppingList, handleRemove, removeAll }) {
     <>
       <div className="header2">
         <h2>Shopping List</h2>
-        <button>Reset</button>
+        <button onClick={() => markAllNotPurchased()}>Reset</button>
         <button onClick={() => removeAll()}>Clear</button>
       </div>
       <div className="list">
@@ -20,17 +20,9 @@ export function ShoppingList({ shoppingList, handleRemove, removeAll }) {
               <span className="item-unit">{val.unit}</span>
             </p>
             <p>{purchasedText}</p>
-            <div
-              className={
-                val.is_purchased
-                  ? setButtonClass("hide-btns") && setPurchasedText("Purchased")
-                  : buttonClass
-              }
-            >
               <button onClick={() => markPurchased(val.id)}>Buy</button>
               <button onClick={() => handleRemove(val.id)}>Remove</button>
             </div>
-          </div>
         ))}
       </div>
     </>
