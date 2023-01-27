79a02d539eea5174f8dc92ab95bb15e58595ae1b
Brock Benson <brockjbenson@outlook.com>
2023-01-27 11:52:30 -0600

    lots of styling

--- src/components/ShoppingList/ShoppingList.jsx
+++ src/components/ShoppingList/ShoppingList.jsx
@@ -7,10 +7,12 @@ export function ShoppingList({
 }) {
   return (
     <>
-      <div className="header2">
-        <h2>Shopping List</h2>
-        <button onClick={() => markAllNotPurchased()}>Reset</button>
-        <button onClick={() => removeAll()}>Clear</button>
+      <div className="header2-container">
+        <div className="header2">
+          <h2>Shopping List</h2>
+          <button onClick={() => markAllNotPurchased()}>Reset</button>
+          <button onClick={() => removeAll()}>Clear</button>
+        </div>
       </div>
       <div className="list">
         {shoppingList.map((val) => {
@@ -21,7 +23,7 @@ export function ShoppingList({
                 <span className="item-quan">{val.quantity}</span>
                 <span className="item-unit">{val.unit}</span>
               </p>
-              {val.is_purchased && <p>Purchased</p>}
+              {val.is_purchased && <p className="purchase-txt">Purchased</p>}
               <div className={val.is_purchased ? "hide-btns" : "list-btns"}>
                 <button onClick={() => markPurchased(val.id)}>Buy</button>
                 <button onClick={() => handleRemove(val.id)}>Remove</button>
