8362b03d3329c944c0ab0cb63c1546f5ca310518
Brock Benson <brockjbenson@outlook.com>
2023-01-27 10:25:27 -0600

    button hide on purchase finished

--- src/components/ShoppingList/ShoppingList.jsx
+++ src/components/ShoppingList/ShoppingList.jsx
@@ -1,9 +1,9 @@
-import { useState } from "react";
-
-export function ShoppingList({ shoppingList, handleRemove, removeAll }) {
-  const [buttonClass, setButtonClass] = useState("list-btns");
-  const [purchasedText, setPurchasedText] = useState("");
-
+export function ShoppingList({
+  shoppingList,
+  handleRemove,
+  removeAll,
+  markPurchased,
+}) {
   return (
     <>
       <div className="header2">
@@ -12,26 +12,26 @@ export function ShoppingList({ shoppingList, handleRemove, removeAll }) {
         <button onClick={() => removeAll()}>Clear</button>
       </div>
       <div className="list">
-        {shoppingList.map((val) => (
-          <div key={val.id} className="list-items">
-            <p className="item-name">{val.item}</p>
-            <p className="item-attr">
-              <span className="item-quan">{val.quantity}</span>
-              <span className="item-unit">{val.unit}</span>
-            </p>
-            <p>{purchasedText}</p>
-            <div
-              className={
-                val.is_purchased
-                  ? setButtonClass("hide-btns") && setPurchasedText("Purchased")
-                  : buttonClass
-              }
-            >
-              <button onClick={() => markPurchased(val.id)}>Buy</button>
-              <button onClick={() => handleRemove(val.id)}>Remove</button>
+        {shoppingList.map((val) => {
+          //   if (val.is_purchased) {
+          //     let purchasedText = "Purchased";
+          //   }
+
+          return (
+            <div key={val.id} className="list-items">
+              <p className="item-name">{val.item}</p>
+              <p className="item-attr">
+                <span className="item-quan">{val.quantity}</span>
+                <span className="item-unit">{val.unit}</span>
+              </p>
+              {val.is_purchased && <p>Purchased</p>}
+              <div className={val.is_purchased ? "hide-btns" : "list-btns"}>
+                <button onClick={() => markPurchased(val.id)}>Buy</button>
+                <button onClick={() => handleRemove(val.id)}>Remove</button>
+              </div>
             </div>
-          </div>
-        ))}
+          );
+        })}
       </div>
     </>
   );
